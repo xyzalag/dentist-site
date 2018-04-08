d9304a129adbe29581e7fb905ee0e2960f909b18
Aga <agnieszkagebus@gmail.com>
2018-04-08 18:34:36 +0200

    add contact section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -8,26 +8,19 @@ import Modal from './modules/Modal';
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
-new RevealOnScroll($(".contact"), "60%");
 var stickyHeader = new StickyHeader();
 var modal = new Modal();
 
-function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
-        var map = new google.maps.Map(document.getElementById('googleMap'), {
-          zoom: 4,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }
+
+// Znany Lekarz - Widget
+!function($_x,_s,id){var js,fjs=$_x.getElementsByTagName(_s)[0];if(!$_x.getElementById(id)){js = $_x.createElement(_s);js.id = id;js.src = "//www.docplanner-platform.com/js/widget.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","zl-widget-s");
 
 jQuery(function ($) {
+
+  'use strict';
    (function () {
       $(document).ready(function(){
-        $('.owl-carousel').owlCarousel({
+        $('.recommendation-carousel').owlCarousel({
             nav: true,
             navText: ["◀","▶"],
             items: 1,
@@ -41,6 +34,22 @@ jQuery(function ($) {
         })
     });
 }());
+
+  (function () {
+      $(document).ready(function(){
+        $('.clinic__carousel').owlCarousel({
+            items: 1,
+            loop:true,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            smartSpeed: 500,
+            autoplayHoverPause:true,
+            margin:10,
+            touchDrag: true,
+        })
+    });
+}());
+
 
 
    (function () {
@@ -58,42 +67,204 @@ jQuery(function ($) {
     });
 }());
 
+(function () {
+        if ($('#googleMap').length > 0) {
 
-//     (function () {
-//       if ($('#googleMap').length > 0) {
+            //set your google maps parameters
+            var $latitude  = 50.024763, //If you unable to find latitude and longitude of your address. Please visit http://www.latlong.net/convert-address-to-lat-long.html you can easily generate.
+                $longitude = 19.224779,
+                $map_zoom  = 16;
+            /* ZOOM SETTING */
 
-//             //set your google maps parameters
-//             var $latitude  = 50.061958, //If you unable to find latitude and longitude of your address. Please visit http://www.latlong.net/convert-address-to-lat-long.html you can easily generate.
-//                 $longitude = 19.919863,
-//                 $map_zoom  = 16;
-//             /* ZOOM SETTING */
+            //google map custom marker icon
+            var $marker_url = './assets/images/google-map-marker.png';
 
-//             //google map custom marker icon
-//             var $marker_url = 'img/google-map-marker.png';
+            //we define here the style of the map
+            var style = [
+            {
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#f5f5f5"
+                  }
+                ]
+              },
+              {
+                "elementType": "labels.icon",
+                "stylers": [
+                  {
+                    "visibility": "off"
+                  }
+                ]
+              },
+              {
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#616161"
+                  }
+                ]
+              },
+              {
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                  {
+                    "color": "#f5f5f5"
+                  }
+                ]
+              },
+              {
+                "featureType": "administrative.land_parcel",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#bdbdbd"
+                  }
+                ]
+              },
+              {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#eeeeee"
+                  }
+                ]
+              },
+              {
+                "featureType": "poi",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#757575"
+                  }
+                ]
+              },
+              {
+                "featureType": "poi.park",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#e5e5e5"
+                  }
+                ]
+              },
+              {
+                "featureType": "poi.park",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#9e9e9e"
+                  }
+                ]
+              },
+              {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#ffffff"
+                  }
+                ]
+              },
+              {
+                "featureType": "road.arterial",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#757575"
+                  }
+                ]
+              },
+              {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#dadada"
+                  }
+                ]
+              },
+              {
+                "featureType": "road.highway",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#616161"
+                  }
+                ]
+              },
+              {
+                "featureType": "road.local",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#9e9e9e"
+                  }
+                ]
+              },
+              {
+                "featureType": "transit.line",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#e5e5e5"
+                  }
+                ]
+              },
+              {
+                "featureType": "transit.station",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#eeeeee"
+                  }
+                ]
+              },
+              {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#c9c9c9"
+                  }
+                ]
+              },
+              {
+                "featureType": "water",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#9e9e9e"
+                  }
+                ]
+              }
+            ];
 
-//             //set google map options
-//             var map_options = {
-//                 center            : new google.maps.LatLng($latitude, $longitude),
-//                 zoom              : $map_zoom,
-//                 panControl        : false,
-//                 zoomControl       : true,
-//                 mapTypeControl    : false,
-//                 streetViewControl : false,
-//                 mapTypeId         : google.maps.MapTypeId.ROADMAP,
-//                 scrollwheel       : false,
-//                 styles            : style,
-//             }
-//             //initialize the map
-//             var map = new google.maps.Map(document.getElementById('googleMap'), map_options);
-//             //add a custom marker to the map
-//             var marker = new google.maps.Marker({
-//                 position : new google.maps.LatLng($latitude, $longitude),
-//                 map      : map,
-//                 visible  : true,
-//                 icon     : $marker_url
-//             });
-//         }
-// }());
+            //set google map options
+            var map_options = {
+                center            : new google.maps.LatLng($latitude, $longitude),
+                zoom              : $map_zoom,
+                panControl        : false,
+                zoomControl       : true,
+                mapTypeControl    : false,
+                streetViewControl : false,
+                mapTypeId         : google.maps.MapTypeId.ROADMAP,
+                scrollwheel       : false,
+                styles            : style,
+            }
+            //initialize the map
+            var map = new google.maps.Map(document.getElementById('googleMap'), map_options);
+            //add a custom marker to the map
+            var marker = new google.maps.Marker({
+                position : new google.maps.LatLng($latitude, $longitude),
+                map      : map,
+                visible  : true,
+                icon     : $marker_url
+            });
+        }
+    }());
+
 
 
 }); // JQuery end
